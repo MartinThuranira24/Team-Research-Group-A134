84ac527f63c2b809fe08250082cca06baf404463
CHINEDU EMMANUEL <ce24aap@herts.ac.uk>
2024-11-19 16:48:20 +0000

    update: variables

--- comparison_of_means.R
+++ comparison_of_means.R
@@ -2,7 +2,7 @@
 ## Importing the dataset
 toyota <- read.csv('toyota.csv')
 #Selecting the dependent and independent variable
-df <- subset(toyota, select = c(tax, mileage))
+df <- subset(toyota, select = c(price, model))
 
 # ------------------------------------------------------------------------------
 ## Visualization plots
@@ -20,7 +20,7 @@ test_set <- subset(df, split == FALSE)
 
 ## Regressor
 model <- lm(
-    formula = tax ~ mileage,
+    formula = price ~ model,
     data = training_set
 )
 summary(model)
