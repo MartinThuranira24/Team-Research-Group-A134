22312b25b4e1e7bd19fda407e6435f9ba110fc76
Martin Thuranira <mb24acz@herts.ac.uk>
2024-11-22 22:17:59 +0000

    added comments

--- Correlation.R
+++ Correlation.R
@@ -6,10 +6,14 @@ hist(toyota$tax, main = "Histogram of road tax of toyota cars", xlab = "Road Tax
 
 # cor.test(toyota$mileage,toyota$tax, method = "spearman")
 
+#genertae scatterplot
+
 plot(toyota$mileage, toyota$tax,
      xlab = "Road Tax in £", ylab =
        "Mileage of vehicle", main
      = "Scatterplot of Road Tax vs Mileage of used Toyota car")
 abline(lm(toyota$tax ~
             toyota$mileage), col = "red")
+
+
 
