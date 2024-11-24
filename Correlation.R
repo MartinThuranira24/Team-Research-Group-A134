1eb7ef808a29d92e6d8dca1058202b24358264b8
Laban Mahihu <lm24aam@herts.ac.uk>
2024-11-24 13:26:20 +0000

    update: correlation code deletion: comparison of means file

--- Correlation.R
+++ Correlation.R
@@ -1,19 +1,45 @@
-hist(toyota$mileage, main = "Histogram of mileage of toyota cars", xlab = "Car mileage")
+# ------------------------------------------------------------------------------
+## Preparing the data set
+toyota_df <- read.csv("toyota.csv")
+toyota_df <- toyota_df[toyota_df$year >= 2010, ]
+data <- subset(toyota_df, select = c(mileage, tax))
 
-# filtered_toyota = subset(toyota,tax>5)
+# ------------------------------------------------------------------------------
+## Tax Histogram
+hist_data <- hist(
+  data$tax,
+  breaks = 7,
+  col = "lightblue",
+  main = "Histogram of Road Tax for used Toyota Cars in the UK (2010 - 2020).",
+  xlab = "Road Tax (£)",
+  border = "black"
+)
+# Calculating the bell curve
+tax_mean <- mean(data$tax, na.rm = TRUE)
+tax_sd <- sd(data$tax, na.rm = TRUE)
+bin_width <- diff(hist_data$breaks)[1]
+x_vals <- seq(
+  min(data$tax, na.rm = TRUE), max(data$tax, na.rm = TRUE),
+  length = 1000
+)
+y_vals <- dnorm(
+  x_vals,
+  mean = tax_mean, sd = tax_sd
+) * bin_width * length(data$tax)
+lines(x_vals, y_vals, col = "red", lwd = 2)
 
-hist(toyota$tax, main = "Histogram of road tax of toyota cars", xlab = "Road Tax")
+# ------------------------------------------------------------------------------
+## Correlation test between tax and mileage
+cor.test(data$mileage, data$tax, method = "kendall")
 
-# cor.test(toyota$mileage,toyota$tax, method = "spearman")
-
-#genertae scatterplot
-
-plot(toyota$mileage, toyota$tax,
-     xlab = "Road Tax in £", ylab =
-       "Mileage of vehicle", main
-     = "Scatterplot of Road Tax vs Mileage of used Toyota car")
-abline(lm(toyota$tax ~
-            toyota$mileage), col = "red")
-
-
-
+# ------------------------------------------------------------------------------
+## Scatter Plot of Tax vs Mileage
+plot(
+  data$mileage, data$tax,
+  main = "Scatterplot of Tax vs Mileage for used Toyota cars in the UK (2010 - 2020).",
+  xlab = "Mileage",
+  ylab = "Road Tax (£)",
+  pch = 19,
+  col = "blue"
+)
+abline(lm(data$tax ~ data$mileage), col = "red")
